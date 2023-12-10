61827a4e49e05a04b946b5dea04631a303c7dd4e
Austin Bayley <austin.bayley@icloud.com>
2023-12-10 23:36:52 +0000

    Policy update

--- src/components/footer/policy.tsx
+++ src/components/footer/policy.tsx
@@ -11,6 +11,9 @@ import {
     Text,
     useColorModeValue,
     Heading,
+    UnorderedList,
+    ListItem,
+    Link,
 } from '@chakra-ui/react'
 import { FaInfo } from 'react-icons/fa'
 import { useLocalStorage } from '../../hooks/useStorage'
@@ -55,33 +58,66 @@ const Policy = () => {
                     </ModalHeader>
 
                     <ModalBody>
-                        <Heading size="sm">General</Heading>
+                        <Heading size="sm">Preamble</Heading>
+                        <UnorderedList mb={2}>
+                            <ListItem>Version: 1.0.0</ListItem>
+                            <ListItem>Last Updated: 10/12/2023</ListItem>
+                        </UnorderedList>
+
                         <Text mb={2}>
                             This website enables user input of "activity"
                             information to track progress against a defined
-                            "target" which is encapsulated by a "challenge".
-                            Currently this is only enabled for myself (Austin
-                            Bayley) & a single challenge. For all users other
-                            than myself (Austin Bayley), there is no expectation
-                            or functionality to input personal information with
-                            personal information only being gathered as part of
-                            the logging process which is described below. Data
-                            is stored securely and no data will be shared or
-                            sold.
+                            "target" which is encapsulated by a "challenge". For
+                            the purpose of this notice, "myself" relates to the
+                            creator of this site, Austin Bayley (contact details
+                            below).
                         </Text>
+
+                        <Heading size="sm">Contact</Heading>
+                        <UnorderedList mb={2}>
+                            <ListItem>Name: Austin Bayley</ListItem>
+                            <ListItem>
+                                Email:{' '}
+                                <Link href="mailto:austinbayley.charity@gmail.com">
+                                    austinbayley.charity@gmail.com
+                                </Link>
+                            </ListItem>
+                        </UnorderedList>
+
+                        <Heading size="sm">General</Heading>
+                        <Text mb={2}>
+                            Functionality is only enabled for myself & a single,
+                            pre-programmed challenge. For all users other than
+                            myself, there is no expectation or functionality to
+                            input personal information with such information
+                            only being gathered as part of the logging process
+                            which is described below. Data is stored securely
+                            which access controls in place. No data will be
+                            shared, sold or used for purposes other than those
+                            outlined below.
+                        </Text>
+
                         <Heading size="sm">Cookies</Heading>
                         <Text mb={2}>
                             Only functional cookies are used on this site.
                         </Text>
+
                         <Heading size="sm">Logging</Heading>
                         <Text mb={2}>
-                            Logs are gathered to prevent abuse & maintain site
-                            security. Logs contain IP address and user ID
-                            information. IP address is the IP address from which
-                            the server recieved a request. For anonymous users
-                            of this platform (anybody that is not logged in -
-                            this is all users by default), user ID will be
-                            "unknown". Logs are automatically deleted after 30
+                            Logs are gathered to prevent abuse & to maintain
+                            data & site security. Logs contain personal
+                            information drawn from requests made to the server,
+                            specifically IP addresses and user account
+                            identifiers. An IP address is the IP address from
+                            which the server recieved a request. A user
+                            identifier is a string that can be used to identify
+                            a user account. For any user that is not logged in
+                            (any user that is not myself), an anonymous user
+                            identifier will be used. It will not be possible to
+                            correlate this anonymous identifier with an
+                            individual. A combination of IP address and user
+                            identifier will be used to determine suspicious
+                            activity. Logs are automatically deleted after 30
                             days.
                         </Text>
                     </ModalBody>
